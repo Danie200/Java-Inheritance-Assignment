32da9c466dda570d362508463456a2139dbf8e56
Daniel Richard <drekwere5@gmail.com>
2025-04-25 22:24:46 +0100

    assignment

---
+++ src/main/java/com/mycompany/hierachicalinheritance/Lion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hierachicalinheritance;
+
+/**
+ *
+ * @author USER
+ */
+public class Lion extends Animal2{
+    public void Roar(){
+  System.out.println("Lions Roar");
+    }
+}
