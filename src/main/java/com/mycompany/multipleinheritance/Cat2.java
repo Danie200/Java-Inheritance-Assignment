7718354544fe696b1f5532302e336646e6d83e5d
Daniel Richard <drekwere5@gmail.com>
2025-04-25 22:07:45 +0100

    assignment

---
+++ src/main/java/com/mycompany/multipleinheritance/Cat2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.multipleinheritance;
+
+/**
+ *
+ * @author USER
+ */
+public class Cat2 extends Anilmal1{
+    public void jumps(){
+        System.out.println("Cats can jump");
+    }
+}
