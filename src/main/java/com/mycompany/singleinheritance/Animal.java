0e16c63d0aca827c7a979b7cf5b0f261d0acd5db
Daniel Richard <drekwere5@gmail.com>
2025-04-25 21:50:37 +0100

    assignment

---
+++ src/main/java/com/mycompany/singleinheritance/Animal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.singleinheritance;
+
+/**
+ *
+ * @author USER
+ */
+public class Animal {
+    public void makeSound(){
+        System.out.println("I am a cat");
+    }
+}
