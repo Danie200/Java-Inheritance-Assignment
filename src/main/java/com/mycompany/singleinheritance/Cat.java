d82666e8ad49f861ea4f74ae394f9bb5851ad956
Daniel Richard <drekwere5@gmail.com>
2025-04-25 21:50:19 +0100

    assignment

---
+++ src/main/java/com/mycompany/singleinheritance/Cat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.singleinheritance;
+
+/**
+ *
+ * @author USER
+ */
+public class Cat extends Animal {
+   public static void main (String[] args){
+       Animal sound = new Animal();
+       sound.makeSound();//Single Inheritance
+   }
+    
+}
