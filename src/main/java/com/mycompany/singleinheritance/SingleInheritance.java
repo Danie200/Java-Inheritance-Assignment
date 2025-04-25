a7eeef64dd8365ef9ca1461948e751390b8e3c71
Daniel Richard <drekwere5@gmail.com>
2025-04-25 21:50:50 +0100

    assignment

---
+++ src/main/java/com/mycompany/singleinheritance/SingleInheritance.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.singleinheritance;
+
+/**
+ *
+ * @author USER
+ */
+public class SingleInheritance {
+
+    public static void main(String[] args) {
+       
+    }
+}
